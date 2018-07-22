bca0f00a8f837058fb632a17d01b8b52e9b52871
jjkyun <jkyunkim01@gmail.com>
2018-07-22 17:09:56 +0900

    final commit

--- dart.py
+++ dart.py
@@ -7,27 +7,31 @@ import requests
 from bs4 import BeautifulSoup
 from pprint import pprint
 
+def getCode(link):
+    response = requests.get('https://dart.fss.or.kr/' + link)
+    bs = BeautifulSoup(response.text, 'html.parser')
+    if bs.find('div', class_="pop_table_B").table.find_all('tr')[3].th.get_text() == '종목코드':
+        ## 현재 4번째 tr이 종목코드이지만, 추후 양식이 바뀔 것을 대비해서 '종목코드' 체크
+        return bs.find('div', class_="pop_table_B").table.find_all('tr')[3].td.get_text()
+
 def requestDart():
     URL = "http://dart.fss.or.kr/dsac001/mainAll.do?selectDate=&sort=&series=&mdayCnt=2" 
     ## mdayCnt=1이 당일을 기준이며 2는 하루 전이다. (주말도 Counting 된다)
     
     response = requests.get(URL)
-    html = response.text
-    parsing = BeautifulSoup(html, 'html.parser')
+    bs = BeautifulSoup(response.text, 'html.parser')
 
-    parsing.find('div', id ='listContents')
-    
-    # listContents = soup.find('div', id='listContents') #.find 함수를 써서 테이블 범위를 지정
-    # listTr = listContents.div.table.find_all('tr') #점점 좁혀나간다
-    # #print(listTr)
-    # for tr in listTr:
-    #     try: #tr이 없는 경우를 생각해서
-    #         time = tr.find('td', class_="cen_txt").get_text().strip() #tr.find는 가장 위에 있는 것만 반환
-    #         company = tr.find('span').a.get_text().strip() #회사 이름만 프린트
-    #         title = tr.find_all('td')[2].get_text().replace(" ","").strip() #find_all하고 인덱싱으로 원하는 td에 접근
-    #         date = tr.find_all('td', class_="cen_txt")[1].get_text()
-    #         print(time+' '+company+' '+title+' '+date)
-    #     except:
-    #         pass
-    
+    listContents = bs.find('div', id ='listContents')
+    listTr = listContents.div.table.find_all('tr')[1:]
+
+    for tr in listTr:
+        time = tr.find('td', class_='cen_txt').get_text().strip()
+        market = tr.find('span').img['title']
+        name = tr.find('span').a.get_text().strip()
+        code = getCode(tr.find('span').a['href'])
+        ## code가 None일 경우는 market이 기타인 경우
+        title = ''.join(tr.find_all('td')[2].get_text().replace(" ","").split())
+
+        print(time, market, name, code, title)
+
 requestDart()
